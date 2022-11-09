18a19d06592a78130aabc23c52b9663fdee26d63
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-11-09 16:38:18 +0000

    updated header and footer styling - App.css, and Header.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 import '../App.css';
 
 class Footer extends React.Component {
@@ -7,7 +8,7 @@ class Footer extends React.Component {
     return (
       <main>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='nav-container'>
-        <Navbar.Brand>Join Us For More Updates</Navbar.Brand>
+        <Navbar.Brand className='footer-text'>Join Us For More Updates</Navbar.Brand>
       
         <Navbar.Brand className='footer-style left'>Jordan Kwan</Navbar.Brand>
         <div className="circle"></div>
@@ -16,7 +17,11 @@ class Footer extends React.Component {
         <Navbar.Brand className='footer-style'>Mandela Steele</Navbar.Brand>
         <div className="circle"></div>
         <Navbar.Brand className='footer-style2'>Nerissa Leynes</Navbar.Brand>
-      </Navbar></main>
+        <Navbar.Brand className='footer-style2 sign-up'>Subscribe Now!</Navbar.Brand>
+        <input className="email-input" placeholder='Enter Your Email' />
+        <Button variant="secondary" className='footerBtn'>Sign-up</Button>{' '}
+      </Navbar>
+      </main>
     )
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
+// import Container from 'react-bootstrap/Container';
 import { Link } from "react-router-dom";
 import '../Header.css';
 
 class Header extends React.Component {
   render() {
     return (
-      <Container className="headerCont">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='header-container'>
         <Navbar.Brand className= "Header">Movie Night</Navbar.Brand>
         <NavItem>
           <Link to="/" className="nav-link">Home</Link>
@@ -18,7 +17,7 @@ class Header extends React.Component {
           <Link to="/about" className="nav-link">About Us</Link>
 
         </NavItem>
-      </Navbar></Container>
+      </Navbar>
     )
   }
 }
