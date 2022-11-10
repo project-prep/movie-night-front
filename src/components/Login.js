d762ab6a0452a27ea0327fadffdb9e60df47c6de
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-11-10 17:01:59 +0000

    changed font size on login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,13 +7,15 @@ const Login = () => {
 
   return (
     <>
-      
+
       <div className="login-div">
         <h1 className='welcome'>Welcome to Movie Night</h1>
         <button className="login-btn" onClick={() => loginWithRedirect()}>Log In</button>
       </div>
       <div className="login-description">
-        <p> Movie night is a web application that allows users to search movies and their streaming information. It saves time and makes it easier for date night choices. It also lets the user keep track of their favorite lists or move movies into the watch list. With over 200 streaming services in the US, Movie Night will save you time. Come and check out our website. </p>
+        <p> Movie night is a web application that allows users to search movies and their streaming information. It saves time and makes it easier for date night choices.</p>
+
+        <p> It also lets the user keep track of their favorite lists or move movies into the watch list. With over 200 streaming services in the US, Movie Night will save you time. Come and check out our website. </p>
 
       </div>
 
