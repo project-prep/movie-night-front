b2ddb5b9c602a2113abe3bd52bccc4033834853c
Jordan <jkwan104@gmail.com>
2022-11-10 16:46:37 -0800

    fixes

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../App.css';
 import { Component } from "react";
+import Header from './Header.js';
+import Footer from './Footer.js';
+import jordan from '../assets/jordan.jpg';
 
 
 class About extends Component {
@@ -9,6 +12,8 @@ class About extends Component {
     /* TODO: render information about the developers */
     return (
       <>
+      <Header>
+      </Header>
         <section className='about-sect top'>
           <div className='about-div'>
             <h1 className='about-page-h1'>About Page</h1>
@@ -17,18 +22,18 @@ class About extends Component {
 
         <section className='flip-sect'>
           <div className='item-divs'>
-          <div className="about-div1">
-            <div className="about-div2">
-              <div className="about-div3">
-                <img src="" alt="Jordan Kwan bio" />
-              </div>
-              <div className="about-div4">
+            <div className="about-div1">
+              <div className="about-div2">
+                <div className="about-div3">
+                  <img src={jordan} alt="Jordan Kwan bio" id='img-about'  />
+                </div>
+                  <div className="about-div4">
                 <h1>Jordan Kwan</h1>
                 <p>..title goes here.. (Software Developer..etc)</p>
                 <p>bio/pitch goes here</p>
+                  </div>
               </div>
             </div>
-          </div>
           </div>
         {/* </section> */}
 
@@ -41,8 +46,8 @@ class About extends Component {
               </div>
               <div className="about-div4">
                 <h1>Marc Wagner</h1>
-                <p>..title goes here.. (Software Developer..etc)</p>
-                <p>bio/pitch goes here</p>
+                <p>Software Developer</p>
+                <p>I retired 2 years ago from the US Army and since then have been a project manager for a construction testing and inspection company.  As a project manager I was able to cover a good portion of our vehicle maintenance costs by identifying an undercharged service and increasing the fee appropriately.  I specifically chose to become a software developer because it allows me to learn new things, I like problem solving, and I want the opportunity to grow professionally.   I have a lot of soft-skills leaders need to be effective; gained from years of managing and training increasingly larger groups.  I’m hoping to find a role in an environment where I can work with a small team and see projects from start to finish.  I want to do that in a company that doesn’t use the phrase ‘that’s the way we’ve always done it’.</p>
               </div>
             </div>
           </div>
@@ -69,9 +74,10 @@ class About extends Component {
         {/* <section className='flip-sect'> */}
         <div className='item-divs'>
           <div className="about-div1">
+            {/* <img src={jordan} alt="Nerissa Leynes bio" id='img-about'  /> */}
             <div className="about-div2">
               <div className="about-div3">
-                <img src="./images/issa4.jpg" alt="Nerissa Leynes bio" id='img-about'  />
+                
               </div>
               <div className="about-div4">
                 <h1>Nerissa Leynes</h1>
@@ -83,7 +89,8 @@ class About extends Component {
         </div>
         
         </section>
-     
+      <Footer>
+      </Footer>
 
       </>
     )
