0dd8e567f2049d22b418a8c91269d19fef46f4a4
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-11-11 01:10:06 +0000

    added update modal, update update functionality

--- src/Main.js
+++ src/Main.js
@@ -1,54 +1,57 @@
 import { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-import AddMovie from './components/AddMovie';
-import Movie from './components/Movie.js';
+import UpdateMovie from './components/UpdateMovie';
+// import Movie from './components/Movie.js';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card';
-import Carousel from 'react-bootstrap/Carousel';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import Carousel from 'react-bootstrap/Carousel';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 
 // import Favorite from './components/Favorite';
 
 class Main extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-        searchQuery: '',
-        movieData: [],
-        title: '',
-        streaming: '',
-        year: '',
-        genre: '',
-        synopsis: '',
-        isWatched: false,
-        status: false,
-        updateModalState: false,
-        modalState: false,
-        imageURL: '',
-        updatedMovie: null,
-        watchList: []
-      }
+    this.state = {
+      searchQuery: '',
+      movieData: [],
+      title: '',
+      streaming: '',
+      year: '',
+      genre: '',
+      synopsis: '',
+      isWatched: false,
+      status: false,
+      // updateModalState: false,
+      modalState: false,
+      imageURL: 'https://images.unsplash.com/photo-1515634928627-2a4e0dae3ddf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+      updatedMovie: null,
+      watchList: [], 
+    }
   }
 
   // handler event to get movie data
   handleMovie = async (event) => {
     event.preventDefault();
-      try {
-        let url = `${process.env.REACT_APP_SERVER}/movies?name=${this.state.searchQuery}`;
-        
-        let movieData = await axios.get(url);
-        console.log('movieData: ', movieData.data);
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/movies?name=${this.state.searchQuery}`;
 
-        // let movie = movieData.data;
+      // let movieData = await axios.get(url);
+      let movieData = await axios.get(url);
 
-        this.setState({
-          title: movieData.data[0].name,
-          streaming: movieData.data[0].locations[0].display_name,
-        })
+
+      console.log('movieResponse: ', movieData.data);
+
+      // let movie = movieData.data;
+
+      this.setState({
+        title: movieData.data[0].name,
+        streaming: movieData.data[0].locations[0].display_name
+      })
 
     } catch (error) {
       console.log('Error: ', error.message);
@@ -58,53 +61,57 @@ class Main extends Component {
     }
   }
 
-    handleMovieAdd = (event) => {
-      event.preventDefault();
-        let newMovie = {
-        title: this.state.title,
-        streaming: this.state.streaming
-      }
-      console.log('newMovie: ', newMovie);
-
-      // this.state.watchList.push(newMovie);
-
-      // this.setState({
-      //   watchList: [...this.state.watchList, newMovie] 
-      // })
-
-      this.postMovie(newMovie);
-
-      console.log(this.state.watchList);
+  handleMovieAdd = (event) => {
+    event.preventDefault();
+    let newMovie = {
+      title: this.state.title,
+      streaming: this.state.streaming
     }
+    console.log('newMovie: ', newMovie);
 
-    // below added for addMovie functionality
-    postMovie = async (newMovie) => {
-      try {
-        let url = `${process.env.REACT_APP_SERVER}/movieList`;
-        let newCreatedMovie = await axios.post(url, newMovie);
+    // this.state.watchList.push(newMovie);
+    console.log(this.state.movieData);
 
-        this.setState({
-          watchList: [...this.state.watchList, newCreatedMovie.data]
-        })
-      } catch(error) {
-        console.log('Error Message: ', error.message)
-      }
+    this.setState({
+      watchList: [...this.state.watchList, newMovie]
+    })
+
+    this.postMovie(newMovie);
+
+    console.log(this.state.watchList);
+  }
+
+  // below added for addMovie functionality
+  postMovie = async (newMovie) => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/movieList`;
+      let newCreatedMovie = await axios.post(url, newMovie);
+
+      this.setState({
+        watchList: [...this.state.watchList, newCreatedMovie.data]
+      })
+    } catch (error) {
+      console.log('Error Message: ', error.message)
     }
- 
+  }
 
-    // >>> ASYNC FUNCTIONS <<<<
+
+  // >>> ASYNC FUNCTIONS <<<<
 
   // delete movie data from db
   deleteMovie = async (movieID) => {
     try {
-      let url = `${process.env.REACT_APP_SERVER}/movieList/${movieID._id}`;
+      let url = `${process.env.REACT_APP_SERVER}/movieList/${movieID}`;
 
       await axios.delete(url);
 
-      let updatedMovie = this.state.movieData.filter(movie => movie._id !== movieID);
+      let updatedMovie = this.state.watchList.filter(movie => movie._id !== movieID);
       this.setState({
-        movieData: updatedMovie
+        watchList: updatedMovie
       });
+
+    console.log('updatedMovie: ', updatedMovie)
+
     } catch (error) {
       console.log(error.message);
     }
@@ -118,12 +125,12 @@ class Main extends Component {
 
       let updatedMovie = await axios.put(url, movieToUpdate);
 
-      let updatedMovieArr = this.state.movieData.map(existingMovie => {
+      let updatedMovieArr = this.state.watchList.map(existingMovie => {
         return existingMovie._id === movieToUpdate._id ? updatedMovie.data : existingMovie;
       });
 
       this.setState({
-        movieData: updatedMovieArr
+        watchList: updatedMovieArr
       })
 
     } catch (error) {
@@ -133,32 +140,60 @@ class Main extends Component {
 
   // get movie data from db
   getMovieData = async () => {
+    console.log('getMovie: tada')
+
     try {
       let movies = await axios.get(`${process.env.REACT_APP_SERVER}/movieList`);
       this.setState({
-        movieData: movies.data
+        watchList: movies.data
       })
+
+      console.log('watchist: ', movies.data)
+
+      // if (this.props.auth0.isAuthenticated) {
+      //   const res = await this.props.auth0.getIdTokenClaims();
+
+      //   const jwt = res.__raw;
+
+      //   console.log('token:  ', jwt);
+
+      //   const config = {
+      //     headers: { "Authorization": `Bearer ${jwt}` },
+      //     method: 'get',
+      //     baseURL: process.env.REACT_APP_SERVER,
+      //     url: '/movieList'
+      //   }
+
+      //   let moviesInfo = await axios(config);
+
+      //   console.log('moviesInfo: ', moviesInfo)
+      //   this.setState({
+      //     watchList: moviesInfo.data
+      //   })
+      // }
+
+
     } catch (error) {
       console.log('Get movie data error: ', error.response);
     }
   }
 
-    // handleClosedModal = () => {
-    //   this.setState({
-    //     modalState: false,
-    //   })
-    // }
+  handleClosedModal = () => {
+    this.setState({
+      modalState: false,
+    })
+  }
 
-    // handleOpenUpModal = (bookObj) => {
-    //   this.setState({
-    //     updateModalState: true,
-    //     updatedBook:bookObj
-    //   })
-    // }
+  handleOpenUpModal = (bookObj) => {
+    this.setState({
+      modalState: true,
+      updatedMovie:bookObj
+    })
+  }
 
-    handleCarouselSelect = (selectedIndex, e) => {
-      this.setState({carouselIndex: selectedIndex});
-    } 
+  handleCarouselSelect = (selectedIndex, e) => {
+    this.setState({ carouselIndex: selectedIndex });
+  }
 
   handleChange = (event) => {
     let { value } = event.target;
@@ -167,9 +202,14 @@ class Main extends Component {
     console.log(value);
   }
 
-    render() {
-      return (
-        <>
+
+  componentDidMount() {
+    this.getMovieData();
+  }
+
+  render() {
+    return (
+      <>
         {/* <form onSubmit={this.getMovieData}>
           <label>Enter Movie Title:
             <input type="text" onInput={this.handleMovieSubmit} />
@@ -186,6 +226,7 @@ class Main extends Component {
           close={this.handleClosedModal}
           postMovie={this.postMovie}
         /> */}
+        <aside>
           <Container className='movieCard'>
             <Form onSubmit={this.handleMovie}>
               <label>Enter Movie Title:
@@ -193,8 +234,9 @@ class Main extends Component {
                 <Button type="submit">Search Movie!</Button>
               </label>
             </Form>
-            <Card className='movieCard' style={{ width: '40rem' }}>
-              <Card.Img variant="top" src={this.state.imageURL} />
+            {/* <Card className='movieCard' style={{ width: '40rem' }}> */}
+            <Card className='movieCard' style={{ width: '25rem', color: '#873434' }}>
+              <Card.Img variant="top" src={this.state.imageURL} style={{ width: '25rem', height: '20rem' }} />
               <Card.Body>
                 <Card.Title>{this.state.title}</Card.Title>
                 <div className='movie'>
@@ -203,29 +245,54 @@ class Main extends Component {
               </Card.Body>
             </Card>
             <Form onSubmit={this.handleMovieAdd}>
-                <Button type="submit">Add Movie to Watchlist</Button>
+              <Button type="submit">Add Movie to Watchlist</Button>
             </Form>
           </Container>
+        </aside>
 
-          <Container className='WatchList'>
-           <h2>Your Watchlist</h2>
-            {this.state.watchList.length ? (
-              <Card>
-                {this.state.watchList.map(element => 
-                  <Card.Body>
-                    <h3>{element.title}</h3>
-                    <h4>{element.streaming}</h4>
-                    <Button onClick={element => this.deleteMovie(element._id) } variant='danger'>Delete Movie</Button>
-                  </Card.Body>
+        <Container className='WatchList'>
+          <h2>Your Watchlist</h2>
+          {this.state.watchList.length ? (
+            
+            <Card>
+              {this.state.watchList.map(element =>              
+                <Card.Body key={element._id}>
+                  <h3>{element.title}</h3>
+                  <h4>{element.streaming}</h4>
+                  <Button onClick={element => this.deleteMovie(element._id)} variant='danger'>Delete Movie</Button>
+
+                  <Button onClick= {()=> this.handleOpenUpModal(element)} variant='info'>Update Movie</Button>
+
+                  {/* <Form.Group controlId="status">
+                  <Form.Check type="checkbox" label="available" defaultValue={this.state.updatedMovie.status} />
+                </Form.Group> */}
+                </Card.Body>
               )}
-              </Card>
-            ) : (
-              <h3>No Movies in your Watchlist :</h3>
-            )}
-         </Container>
-        </>
-      );
-    }
+            </Card>
+            
+          ) : (
+            <h3>No Movies in your Watchlist :</h3>
+          )}
+        </Container>
+          
+        { this.state.updatedMovie &&
+          <UpdateMovie
+            show={this.state.modalState}
+            close = {this.handleClosedModal}
+            updateMovie={this.updateMovie}
+            books = {this.state.books}
+            updatedMovie ={this.state.updatedMovie} 
+          />
+        }
+        
+        {/* <ul>
+          {this.state.watchList.map(element => (
+            <li key={element._id}>{element.title}</li>
+          ))}
+      </ul> */}
+      </>
+    );
+  }
 }
 
 export default Main;
